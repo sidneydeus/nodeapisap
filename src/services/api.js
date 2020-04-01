2c9e1fa52491c5a64e4c2d0d557905da51b97da8
Sidney Deus <sidney.deus@gmail.com>
2020-04-01 11:40:36 -0300

    Configuracao PORT

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.github.com/",
   headers: {
-    Authorization: "6309ae2ecb6048d08e1d153135c49327e316f6d1"
+    Authorization: process.env.GITHUB_PERSONAL_TOKEN
   }
 });
 
